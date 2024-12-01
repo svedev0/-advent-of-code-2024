9a4bf5ae35d6c43f03e601078fa88b4ca6c8dcb6
svedev0 <emanuel@kulich.me>
2024-12-01 14:15:01 +0100

    Day01

---
+++ Day01/Day01.cs
@@ -0,0 +1,48 @@
+﻿namespace advent_of_code_2024;
+
+public class Day01
+{
+	public static void SolvePart1()
+	{
+		List<string> input = [.. File.ReadAllLines("Day01\\input.txt")];
+
+		int[] leftVals = [.. input.Select(x => int.Parse(x.Split(" ")[0]))
+			.OrderBy(x => x)];
+
+		int[] rightVals = [.. input.Select(x => int.Parse(x.Split(" ")[^1]))
+			.OrderBy(x => x)];
+
+		List<int> distances = [];
+		for (int i = 0; i < leftVals.Length; i++)
+		{
+			int diff = leftVals[i] - rightVals[i];
+			if (diff < 0)
+			{
+				diff = -diff;
+			}
+			distances.Add(diff);
+		}
+
+		Console.WriteLine($"[Part 1] Answer: {distances.Sum()}");
+	}
+
+	public static void SolvePart2()
+	{
+		List<string> input = [.. File.ReadAllLines("Day01\\input.txt")];
+
+		int[] leftVals = [.. input.Select(x => int.Parse(x.Split(" ")[0]))];
+		int[] rightVals = [.. input.Select(x => int.Parse(x.Split(" ")[^1]))];
+
+		int similarityScore = 0;
+		foreach (int val in leftVals)
+		{
+			int count = rightVals.Count(x => x == val);
+			if (count > 0)
+			{
+				similarityScore += val * count;
+			}
+		}
+
+		Console.WriteLine($"[Part 2] Answer: {similarityScore}");
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace advent_of_code_2024;
+
+public class Program
+{
+	private static readonly int DAY = 1;
+
+	public static void Main(string[] _)
+	{
+		long startTime = Stopwatch.GetTimestamp();
+		Welcome();
+
+		switch (DAY)
+		{
+			case 1:
+				Day01.SolvePart1();
+				Day01.SolvePart2();
+				break;
+			default:
+				throw new Exception("Invalid day");
+		}
+
+		TimeSpan elapsed = Stopwatch.GetElapsedTime(startTime);
+		int elapsedMs = (int)Math.Floor(elapsed.TotalMilliseconds);
+		Console.WriteLine($"The program finished in {elapsedMs} ms");
+	}
+
+	private static void Welcome()
+	{
+		string messsage = $"""
+			===== Advent of Code 2024 =====
+
+			Day {DAY}:
+
+			""";
+		Console.WriteLine(messsage);
+	}
+}
